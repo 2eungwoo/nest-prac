983775b62ff8329fcf8627c550b14ca47efc7765
swlee <swo98@naver.com>
2025-08-05 18:56:13 +0900

    add: validator 모듈 추가

--- src/presentation/users/users.module.ts
+++ src/presentation/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersController } from './users.controller';
 import { User } from '../../domain/users/entities/user.entity';
 import { UserRepository } from '../../domain/users/repositories/user.repository';
 import {UserAuthService} from "../../application/users/service/user-auth.service";
+import {UserAuthValidator} from "../../application/users/validation/user-auth.validator";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UserRepository, UserAuthService],
+  providers: [UserRepository, UserAuthService, UserAuthValidator],
 })
 export class UsersModule {}
