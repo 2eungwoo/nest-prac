87a8e6427df5b2182ceddfb40b8fedd5aabc8e0f
swlee <swo98@naver.com>
2025-08-06 00:31:24 +0900

    fix: 커스텀익셉션 더 다양하게 받을 수 있도록 오버로딩

--- src/global/exception/custom.exception.ts
+++ src/global/exception/custom.exception.ts
@@ -1,12 +1,19 @@
-import { HttpException } from '@nestjs/common';
+import {HttpException} from '@nestjs/common';
 import {ResponseCode} from "../interfaces/response-code.interface";
 
 export class CustomException extends HttpException {
-  constructor(responseCode: ResponseCode, status: number) {
+  constructor(responseCode: ResponseCode, status?: number);
+  constructor(responseCode: ResponseCode, status: number, detail?: any);
+  constructor(responseCode: ResponseCode, statusOrDetail?: number | any, maybeDetail?: any) {
+    const isStatusNumber = typeof statusOrDetail === 'number';
+    const status = isStatusNumber ? statusOrDetail : responseCode.status;
+    const detail = isStatusNumber ? maybeDetail : statusOrDetail;
+
     super(
         {
           code: responseCode.code,
           message: responseCode.message,
+          ...(detail && { detail }), // detail이 있을 경우 포함
         },
         status,
     );
